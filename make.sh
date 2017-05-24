1d87a6bc448ca091bc36dbb9240e3369c4b01161
mnaselli <marty.naselli@gmail.com>
2017-05-24 16:39:43 -0400

    fixed make script

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 # Concat settings.ini with beacons.ini, write to Game.ini
-cat settings.ini beacons.ini > Game.ini
+cat settings.ini > Game.ini
+tail -n +2 beacons.ini >> Game.ini
 
 # Prettify custom_beacons.beacons and store in beacons.json
 python3 -c 'import sys, json; open("beacons.json", "w").write(json.dumps(json.loads(open("custom.beacon").read()),indent=2))'
